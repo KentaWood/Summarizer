5bf0c358e14a868e876b935ec0e580e6e25252c3
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2024-09-09 09:55:11 -0700

    doctest test case in actions

--- docSum.py
+++ docSum.py
@@ -51,7 +51,7 @@ while(len(paragraphs) > 1):
     for paragraph in paragraphs:
         
         # Create a chat completion using the Groq API, sending a system instruction and the user input
-        print('hi')
+        # print('hi')
         chat_completion = client.chat.completions.create(
             messages=[
                 {
@@ -74,7 +74,7 @@ while(len(paragraphs) > 1):
 
     # Re-split the summarized text into paragraphs
     paragraphs = split_document_into_chunks(summarized_text)
-    print(len(paragraphs))  
+    # print(len(paragraphs))  
 
 
 print(paragraph)
